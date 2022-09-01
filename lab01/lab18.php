60bfea72f9e404e08c1b82d9c02881187e348ae9
geekboypa <68137140+geekboypa@users.noreply.github.com>
2022-08-31 21:05:02 -0500

    lab1

---
+++ lab01/lab18.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+        $a = 8;
+        $b = 3;
+        echo $a + $b, "<br>";
+        echo $a - $b, "<br>";
+        echo $a * $b, "<br>";
+        echo $a / $b, "<br>";
+        $a++; echo $a, "<br>";
+        ?>
+    </body>
+</html>
