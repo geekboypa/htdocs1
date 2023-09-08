0273df43b7f178c3ce187630f27c396fd22253e2
geekboypa <68137140+geekboypa@users.noreply.github.com>
2023-09-07 22:13:58 -0500

    Updates lab04.php

---
+++ lab04/lab04.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Indicador de Ventas</title>
+</head>
+<body>
+    <h1>Indicador de Ventas</h1>
+    <form method="post">
+        <label for="ventas">Porcentaje de Ventas:</label>
+        <input type="number" name="ventas" id="ventas" min="0" max="100" required>
+        <input type="submit" value="Calcular">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $ventas = $_POST["ventas"];
+
+        if ($ventas > 80) {
+            $imagen = "semaforo_verde.png";
+            $mensaje = "¡Feliz!";
+        } elseif ($ventas >= 50 && $ventas <= 79) {
+            $imagen = "semaforo_amarillo.png";
+            $mensaje = "Aburrido";
+        } else {
+            $imagen = "semaforo_rojo.png";
+            $mensaje = "Triste";
+        }
+
+        echo "<p>Tu desempeño de ventas está en $ventas%. Estás $mensaje.</p>";
+        echo "<img src='img/$imagen' alt='$mensaje'>";
+    }
+    ?>
+</body>
+</html>
