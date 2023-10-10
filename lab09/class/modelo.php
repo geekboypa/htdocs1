28fa8bf706d512604038cc53002c6d3905461cb0
geekboypa <68137140+geekboypa@users.noreply.github.com>
2023-10-10 18:24:23 -0500

    Update modelo.php

--- lab09/class/modelo.php
+++ lab09/class/modelo.php
@@ -7,7 +7,7 @@ class modeloCredencialesBD
     {
         $this->_db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
         if ($this->_db->connect_errno) {
-            echo "fallo al conectar a la base de datos " . $this->db->connect_errno;
+            echo "fallo al conectar a la base de datos " . $this->_db->connect_errno;
             return;
         }
     }
